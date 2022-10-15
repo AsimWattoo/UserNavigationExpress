7d0adc45bd7b94011d8d273173a956e8ae8be09d
asim <asimwattoo6@gmail.com>
2022-10-15 15:43:09 +0500

    Added router in the App.js file for all the links. Added register router

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/registerRouter');
 
 var app = express();
 
@@ -19,8 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', indexRouter);
+app.use("/signup", registerRouter);
+app.use("/admin/view", adminRouter);
+app.use('/clock', clockRouter);
+app.use('/2fa', authenticationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
